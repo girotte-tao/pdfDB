248522f8f6f7c158f327f76f5702979ace8bd853
girotte-tao <taoware@qq.com>
2024-07-03 14:19:02 +0800

    add tqmd

--- pdf_Extraction/extraction_demo.py
+++ pdf_Extraction/extraction_demo.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import tqdm
 from extract_image import extract_elements_with_images_from_pdf, save_images_by_elements
 from extract_table import extract_tables_from_tei, save_tables_to_xml
 
@@ -8,8 +9,9 @@ zoom_level = 4  # Adjust the zoom level to increase resolution
 base_directory = "../files"
 
 def process_files_in_directory(tei_directory, pdf_directory, output_directory, zoom=2):
+    files = os.listdir(tei_directory)
 
-    for i, file in enumerate(os.listdir(tei_directory)):
+    for i, file in enumerate(tqdm(files, desc="Processing files", unit="file")):
         print(f"Processing file {file}...")
 
         tei_file = os.path.join(tei_directory, file)
@@ -22,6 +24,7 @@ def process_files_in_directory(tei_directory, pdf_directory, output_directory, z
         tables = extract_tables_from_tei(tei_file)
         save_tables_to_xml(tables, os.path.join(output_directory, filename, 'tables'), pdf_id=i)
 
+        print(f"Processed file {file}.")
 
 
 if __name__ == "__main__":
